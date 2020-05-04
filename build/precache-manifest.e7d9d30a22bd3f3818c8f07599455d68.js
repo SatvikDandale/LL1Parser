ba7a924ecdd34e5b92d6726d96af93e35aabb38d
SatvikDandale <satvik.dandale@gmail.com>
2020-05-04 20:23:22 +0530

    Project Done

---
+++ build/precache-manifest.e7d9d30a22bd3f3818c8f07599455d68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4928c9f37f2fcc51b57643bee8d02b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f15b0cedd89af8aa765",
+    "url": "/static/css/main.ccdb01ba.chunk.css"
+  },
+  {
+    "revision": "6e044c2f366a6b6d6744",
+    "url": "/static/js/2.ddd297a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddd297a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f15b0cedd89af8aa765",
+    "url": "/static/js/main.91ff629e.chunk.js"
+  },
+  {
+    "revision": "62d3a822acc96e7e8edc",
+    "url": "/static/js/runtime-main.58010b3f.js"
+  }
+]);
